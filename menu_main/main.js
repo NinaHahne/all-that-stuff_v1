d3de551a77cd2b7544360691f09b870647a9208c
Nina Hahne <ninamariehahne@gmail.com>
2019-12-27 15:12:37 +0100

    added img draggable = 'false' for all browsers

--- menu_main/main.js
+++ menu_main/main.js
@@ -6,6 +6,32 @@
 // limit object drag to window borders?
 // might wanna use .switchClass() from jquery ui?? (instead of .removeClass('v1').addClass('v2')
 
+// set img draggable = 'false' for all browsers - code from https://www.redips.net/firefox/disable-image-dragging/
+
+// register onLoad event with anonymous function
+window.onload = function (e) {
+    var evt = e || window.event,// define event (cross browser)
+        imgs,                   // images collection
+        i;                      // used in local loop
+    // if preventDefault exists, then define onmousedown event handlers
+    if (evt.preventDefault) {
+        // collect all images on the page
+        imgs = document.getElementsByTagName('img');
+        // loop through fetched images
+        for (i = 0; i < imgs.length; i++) {
+            // and define onmousedown event handler
+            imgs[i].onmousedown = disableDragging;
+        }
+    }
+};
+
+// disable image dragging
+function disableDragging(e) {
+    e.preventDefault();
+}
+
+// ------------------------------------
+
 const objObj = [
     {
         "name": "banana",
