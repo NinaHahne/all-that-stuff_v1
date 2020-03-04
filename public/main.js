659b77e32e6afcfcfcd359154622573789b7e653
Nina Hahne <ninamariehahne@gmail.com>
2020-03-04 03:41:04 +0100

    added some style adjustments for mobile

--- public/main.js
+++ public/main.js
@@ -2,31 +2,32 @@
 // var socket = io();
 var socket = io.connect();
 
+const testingMode = true;
 
 // to prevent image dragging for imgs that are dynamically declared:
-// // register onLoad event with anonymous function
-// window.onload = function (e) {
-//     let evt = e || window.event,// define event (cross browser)
-//         imgs,                   // images collection
-//         i;                      // used in local loop
-//     // if preventDefault exists, then define onmousedown event handlers
-//     if (evt.preventDefault) {
-//         // collect all images on the page
-//         imgs = document.getElementsByTagName('img');
-//         // loop through fetched images
-//         for (i = 0; i < imgs.length; i++) {
-//             // and define onmousedown event handler
-//             imgs[i].onmousedown = disableDragging;
-//         }
-//     }
-// };
-//
-// // disable image dragging
-// function disableDragging(e) {
-//     e.preventDefault();
-// }
+// register onLoad event with anonymous function
+window.onload = function (e) {
+    let evt = e || window.event,// define event (cross browser)
+        imgs,                   // images collection
+        i;                      // used in local loop
+    // if preventDefault exists, then define onmousedown event handlers
+    if (evt.preventDefault) {
+        // collect all images on the page
+        imgs = document.getElementsByTagName('img');
+        // loop through fetched images
+        for (i = 0; i < imgs.length; i++) {
+            // and define onmousedown event handler
+            imgs[i].onmousedown = disableDragging;
+        }
+    }
+};
+
+// disable image dragging
+function disableDragging(e) {
+    e.preventDefault();
+}
+
 // ------------------------------------
-const testingMode = true;
 
 // §§ ELEMENTS & GLOBAL VARIABLES ********************************
 
